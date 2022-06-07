2d594efeeb176f54dc4aebb43b876ebc94a21d82
Les Woodland <leswoodland88@gmail.com>
2022-06-07 13:03:02 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -4,38 +4,61 @@ import "./App.css"
 
 // Axios is a promise-based HTTP Client that uses XMLHttpRequests and automatically handles JSON
 const client = axios.create({
-  baseURL: "https://gdev.vigil.com/vigil_test",
-  headers: {'Access-Control-Allow-Origin': null}
+  baseURL: "https://gdev.vigil.com/vigil_test?username=les&token=vigiltoken",
+  withCredentials: false
+  
+  
+
 });
+
+
+// const Http = new XMLHttpRequest();
+// const url='https://gdev.vigil.com/vigil_test?username=les&token=vigiltoken';
+// Http.open("GET", url);
+// Http.send();
+
+// Http.onreadystatechange = (e) => {
+//   console.log(Http.responseText)
+// }
 
 
 const App = () => {
 
   const [data, setData] = useState([]);
-  
+
   const [token, setToken] = useState('')
+  const [magicToken, setMagicToken] = useState('')
 
   useEffect(() => {
-    client.get(`?username=les&token=vigiltoken`).then((response) => {
-      
-      setToken(response.data.token.toUpperCase());
-    });
+    client
+      .get()
+      .then((response) => {
+        setToken(response.data.magic_token.toUpperCase());
+      });
+    updateToken();
   }, []);
 
-  
+  const updateToken = (token) => {
+    client
+      .post('https://gdev.vigil.com/vigil_test', { username: 'les', token:'vigiltoken',result: token })
+      .then((response) => {
+        setMagicToken([response.data.token]);
+      });
+
+  };
 
   return (
     <div className="App-header">
       <h1> Vigil Code Test</h1>
-     
-          <div>
 
-            <p >{token}</p>
- 
-          </div>
-       
-    
-      
+      <div>
+
+      <p>{token}</p>
+
+      </div>
+
+
+
     </div>
   )
 }
