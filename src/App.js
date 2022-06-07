0404849cd3ad499933cb2e7486b2ae332cbdc4a0
Les Woodland <leswoodland88@gmail.com>
2022-06-07 09:34:32 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import "./App.css"
 
-function App() {
+// Axios is a promise-based HTTP Client that uses XMLHttpRequests and automatically handles JSON
+const client = axios.create({
+  baseURL: "https://dynggo.herokuapp.com/"
+});
+
+const App = () => {
+
+  const [data, setData] = useState([]);
+  const [id, setId] = useState('');
+
+  useEffect(() => {
+    client.get(`reports?identifier=${id}`).then((response) => {
+      setData(response.data);
+    });
+  }, [id]);
+
+  const handleId = (e) => {
+    e.preventDefault();
+    setId(e.target[0].value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Vigil Code Test</h1>
+      <form onSubmit={handleId}><input placeholder='Enter id#' type="text" id="fname" name="fname" ></input></form>
+      {data.map((data) => {
+        return (
+          <div className="post-card" key={data}>
+
+            <h2 className="post-title">{data.identifier}</h2>
+            <p className="post-body">{data._id}</p>
+
+          </div>
+        );
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
